fb429da8c98054cf8e689088e885a69dd8d7d2f2
Sony <sonysunny028@gmail.com>
2022-12-07 06:10:58 -0400

    modified template folder and added rtos multitask project

--- ESPIDF_RTOS_Template/main/multitask_main.c
+++ ESPIDF_RTOS_Template/main/multitask_main.c
@@ -4,32 +4,21 @@
 #include "freertos/task.h"
 
 
-TaskHandle_t myTask1Handle = NULL;
-TaskHandle_t myTask2Handle = NULL;
+TaskHandle_t myTask1Handle = NULL;     //creating task1 handle
+TaskHandle_t myTask2Handle = NULL;     //creating task2 handle
 
 int i=0;
 void task1(void *arg){
-    while(1){
-        i=xTaskGetTickCount();
-        printf("From task1!\n");
+    /*Write task1 code here*/
+    while (1){
+        printf("from task1!\n");
         vTaskDelay(1000/portTICK_RATE_MS);
-        if(i==300){
-            vTaskSuspend(myTask2Handle);
-            printf("Task2 suspended!\n");
-        }
-        if(i==500){
-            vTaskResume(myTask2Handle);
-            printf("Task2 Resumed!\n");
-        }
-        if(i==700){
-            vTaskDelete(myTask2Handle);
-            printf("Task2 Deleted!\n");
-        }
     }
     
 }
 
 void task2(void *arg){
+    /*write task2 code here*/
     while (1){
         printf("from task2!\n");
         vTaskDelay(1000/portTICK_RATE_MS);
@@ -41,7 +30,7 @@ void task2(void *arg){
 
 
 void app_main(){
-    xTaskCreate(task1, "task1", 4096, NULL, 10, &myTask1Handle);
-    xTaskCreate(task2, "Task2", 4096, NULL, 10, &myTask2Handle);
+    xTaskCreate(task1, "task1", 4096, NULL, 10, &myTask1Handle);  //creating task1
+    xTaskCreate(task2, "Task2", 4096, NULL, 10, &myTask2Handle);  //creating task2
 
 }
